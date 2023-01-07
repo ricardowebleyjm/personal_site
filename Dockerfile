c39ab23d992b7300829cffd5d734b4a782b0b59b
Ricardo Webley <ricardo.webley@integrity.gov.jm>
2023-01-07 10:02:32 -0500

    first commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.11-slim
+
+RUN apt-get update
+RUN apt-get -y install curl
+RUN curl -fsSL https://deb.nodesource.com/setup_14.x | bash - &&\
+    apt-get install -y nodejs
+
+WORKDIR /code
+
+COPY requirements.txt /code/
+COPY . ./code
+
+RUN pip install -r requirements.txt
+
+EXPOSE 9090
+ENTRYPOINT ["sh", "entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Running Database migrations
+# python manage.py migrate --noinput
+chown -R 1000:1000 "/root/.npm"
+python manage.py collectstatic --noinput --clear
+python manage.py tailwind build
+
+
+
+# Running Application with uWSGI
+exec gunicorn personal_site.wsgi:application \
+    --bind=0.0.0.0:9090 \
+    # --log-level=info \
+    # --access-logfile=$PWD/accesslog.log \
+    # --workers=4 \
+    # --error-logfile=$PWD/errorlog.log \
